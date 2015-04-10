59295e2664eb306c697d1091a7a3779245feaab7
baallezx <abalzer22@gmail.com>
2015-04-09 17:02:41 -0700

    adding a basic template for what the check file, and parse dir methods will look like.

--- multi_nodes.py
+++ multi_nodes.py
@@ -31,7 +31,10 @@ import re
 __author__ = "Alex Balzer"
 __version__ = "0.1.0"
 
+# TODO: need to create the method that will search the `dirs` that you specify
+
 def generate_json_file():
+	# TODO:
 	# generates a base image for the json file
 	pass
 
@@ -39,10 +42,9 @@ def connect_to_server(server_name, uname, passwd, key=None):
 	ssh = pm.SSHClient()
 	try:
 		if key == None:
-			print 'a'
 			ssh.set_missing_host_key_policy(pm.AutoAddPolicy())
 			ssh.connect( server_name, username=uname, password=passwd )
-			print ssh
+			# print ssh
 			return ssh
 		else:
 			ssh.set_missing_host_key_policy(pm.AutoAddPolicy())
@@ -51,9 +53,49 @@ def connect_to_server(server_name, uname, passwd, key=None):
 	except:
 		return None
 
-def search_dir(directory, search_string=None, regex_pattern=None):
+class fileinfo(object):
+	"""
+	basic fileinfo object that gives all the relative information
+	"""
+	def __init__(self,filename,filedata):
+		self.filename = filename
+		self.filedata = filedata
+		self.important_lines = []
+
+	def update_lines(self,n):
+		"""
+		add a line number with relative information
+		"""
+		self.important_lines.append(n)
+
+def _check_file(filepath, search_string, regex_pattern):
+	# TODO: implement this function to open a file and iterate through it. if you find a match then retrun those results.
+	r = open(filepath,'r').readlines()
+	results = []
+	for i in r:
+		if search_string == None:
+			pass
+		else:
+			if search_string in i:
+				results.append(i)
+		
+
+def search_dir(directory, search_string=None, regex_pattern=None,file_types=None):
+	"""
+	file_types = None means that you search all files you come across
+	"""
+	# TODO: create a non-recursive solution to this. in case you only want to search that level of the directory
 	for root, dirs, files in os.walk(directory):
-		pass
+		for i in files:
+			if file_types == None:
+				# check all files
+				_check_file(os.path.join(root,i) , search_string , regex_pattern)
+			elif isinstance(file_types, list):
+				for ftype in file_types:
+					if i.endswith(ftype):
+						_check_file( os.path.join(root,i) , search_string , regex_pattern )
+			else:
+				return Exception # TODO: need to create a custom exception here.
 
 # --------------------------------------------------------------------------------------------------
 # borrowed from "http://sentdex.com/sentiment-analysisbig-data-and-python-tutorials-algorithmic-trading/encryption-and-decryption-in-python-code-example-with-explanation/"
@@ -88,6 +130,10 @@ def decrypt(data,key='not_random'):
 
 # --------------------------------------------------------------------------------------------------
 
+def execute_command(ssh, command):
+	i,o,e = ssh.exec_command(command)
+	# TODO: check all three values and handle accordingly
+
 if __name__ == "__main__":
 	import sys
 	l = sys.argv[1:] # list of all the nodes to connect to
@@ -99,6 +145,7 @@ if __name__ == "__main__":
 			# ssh_conn = connect_to_server()
 			# search_dir()
 		else:
+			# TODO: need to come up with a better way to generate and make sense of these attributs. currently its to flat.
 			print node, json_file[node]['uname'] , json_file[node]['passwd']
 			# decrypt the password
 			key = raw_input("please enter in the file to decrypt your password:\n")
@@ -109,6 +156,11 @@ if __name__ == "__main__":
 			print 'length of the key = ',len(key_file), '\nkey = ',key_file
 			json_file[node]['passwd'] = decrypt( json_file[node]['passwd'] , key_file )
 			ssh = connect_to_server(node, json_file[node]['uname'] , json_file[node]['passwd'])
+			dirs = ['/']
+			if 'dirs' in json_file[node]:
+				dirs = json_file[node]['dirs'] # need to make sure that this is a list
+			for i in dirs:
+				search_dir(i)
 			i,o,e = ssh.exec_command('ls -la')
 			print o.read()
 			print ssh
